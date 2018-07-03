5f3e927957f0ae18f6f7bd3a8063731ab6852704
Kirill <k.shiryaev@corp.rtln.ru>
2018-07-03 12:58:01 +0300

    Add no XML CacheConfig

---
+++ src/main/java/app/config/CacheConfig.java
@@ -0,0 +1,34 @@
+package app.config;
+
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.config.CacheConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+      @Bean(destroyMethod="shutdown")
+      public CacheManager ehCacheManager() {
+          CacheConfiguration cacheConfiguration = new CacheConfiguration();
+          cacheConfiguration.setName("tokenCache");
+          cacheConfiguration.setMaxEntriesLocalHeap(1000);
+          cacheConfiguration.setTimeToIdleSeconds(0);
+          cacheConfiguration.setTimeToLiveSeconds(60);
+          cacheConfiguration.setMemoryStoreEvictionPolicy("CLOCK");
+
+          net.sf.ehcache.config.Configuration config = new net.sf.ehcache.config.Configuration();
+          config.addCache(cacheConfiguration);
+
+          return CacheManager.newInstance(config);
+      }
+
+      @Bean
+      public org.springframework.cache.CacheManager cacheManager() {
+          return new EhCacheCacheManager(ehCacheManager());
+      }
+
+}
--- src/main/java/app/controller/InputController.java
+++ src/main/java/app/controller/InputController.java
@@ -1,10 +1,9 @@
 package app.controller;
 
 import net.sf.ehcache.Cache;
+import net.sf.ehcache.CacheManager;
 import net.sf.ehcache.Element;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.cache.ehcache.EhCacheCacheManager;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,13 +11,13 @@ import org.springframework.web.bind.annotation.*;
 public class InputController {
 
     @Autowired
-    private EhCacheCacheManager ehCacheCacheManager;
+    private CacheManager cacheManager;
 
     @RequestMapping(value = "/auth", method = RequestMethod.GET)
     @ResponseStatus(value = HttpStatus.OK)
     public void inputSignal(@RequestParam(value = "name") String name) {
+        Cache cache = cacheManager.getCache("tokenCache");
         Element element = new Element(name, name + 123);
-        Cache cache = ehCacheCacheManager.getCacheManager().getCache("tokenCache");
         cache.put(element);
         System.out.println(cache.get(name));
     }
